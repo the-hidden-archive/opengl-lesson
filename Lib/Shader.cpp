aa93f05f8070fadcd6a06cd7e51483d0438df3cf
Smirnov Peter Alekseevich <smipealex@yandex.ru>
2022-01-18 08:57:53 +0300

    Add Shader class

---
+++ Lib/Shader.cpp
@@ -0,0 +1,89 @@
+//
+// Created by Пётр Смирнов on 17.01.2022.
+//
+
+#include "Shader.h"
+
+Shader::Shader(const char *vertexPath, const char *fragmentPath) {
+    std::string vertexCode, fragmentCode; // source
+    std::ifstream vertexShaderFile, fragmentShaderFile;
+
+    vertexShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fragmentShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+    try{
+        vertexShaderFile.open(vertexPath);
+        fragmentShaderFile.open(fragmentPath);
+
+        std::stringstream vertexShaderStream, fragmentShaderStream;
+
+        // raed
+        vertexShaderStream << vertexShaderFile.rdbuf();
+        fragmentShaderStream << fragmentShaderFile.rdbuf();
+
+        vertexShaderFile.close();
+        fragmentShaderFile.close();
+
+        vertexCode = vertexShaderStream.str();
+        fragmentCode = fragmentShaderStream.str();
+    } catch (std::ifstream::failure e){
+        std::cerr << "Error: in Shader in reading shader files is fail" << std::endl << e.what() << std::endl;
+    }
+
+    const char* vertexShaderCode = vertexCode.c_str();
+    const char* fragmentShaderCode = fragmentCode.c_str();
+
+    unsigned int vertex, fragment;
+    int success;
+    char infoLog[512];
+
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vertexShaderCode, nullptr);
+    glCompileShader(vertex);
+
+    glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+    if(!success){
+        glGetShaderInfoLog(vertex, 512, nullptr, infoLog);
+        std::cerr << "Error: compiling has fail in Vertex Shader" << std::endl << infoLog << std::endl;
+    }
+
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fragmentShaderCode, nullptr);
+    glCompileShader(fragment);
+
+    glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+    if(!success){
+        glGetShaderInfoLog(fragment, 512, nullptr, infoLog);
+        std::cerr << "Error: compiling has fail in Fragment Shader" << std::endl << infoLog << std::endl;
+    }
+
+    ID = glCreateProgram();
+    glAttachShader(ID, vertex);
+    glAttachShader(ID, fragment);
+    glLinkProgram(ID);
+
+    glGetProgramiv(ID, GL_LINK_STATUS, &success);
+    if(!success){
+        glGetProgramInfoLog(ID, 512, nullptr, infoLog);
+        std::cout << "Error: in linking program" << std::endl << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+}
+
+void Shader::use() {
+    glUseProgram(ID);
+}
+
+void Shader::setBool(const std::string &name, bool value) const {
+    setInt(name, (int)value);
+}
+
+void Shader::setInt(const std::string &name, int value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setFloat(const std::string &name, float value) const {
+    glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
---
+++ Lib/Shader.h
@@ -0,0 +1,25 @@
+//
+// Created by Пётр Смирнов on 17.01.2022.
+//
+
+#ifndef OPENGL_LESSON_SHADER_H
+#define OPENGL_LESSON_SHADER_H
+#include <glad/glad.h>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+class Shader {
+public:
+    unsigned int ID; // shader program id
+    Shader(const char* vertexPath, const char* fragmentPath);
+    void use();
+
+    void setBool(const std::string &name, bool value) const;
+    void setInt(const std::string &name, int value) const;
+    void setFloat(const std::string &name, float value) const;
+};
+
+
+#endif //OPENGL_LESSON_SHADER_H
